5d92faec76338cb60e954a72989aaed0719d1df2
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-12-15 07:28:34 -0600

    Login function add

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
@@ -32,7 +32,8 @@ def signup(request):
             'form': UserCreationForm,
             'error': 'password do not mach'
         })
-    
+
+
 def tasks(request):
     return render(request, 'tasks.html')
 
@@ -40,3 +41,20 @@ def tasks(request):
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password do not match'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
