1fe47a11064b2ee8717f74f9d8ed402259c37f4b
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-12-15 19:52:22 -0600

    Details initial add

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -79,5 +79,9 @@ def createtask(request):
                 'form': TaskForm,
                 'error': 'Bad data passed in. Try again'
             })
+        
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
 
     
